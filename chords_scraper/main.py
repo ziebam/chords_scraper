365e6a98113506ebb9fbbc0daabfabce4a7cbd21
ziebam <ziebamichal@tutanota.com>
2020-09-09 17:43:30 +0200

    Add support for ukutabs.com

--- chords_scraper/main.py
+++ chords_scraper/main.py
@@ -1,5 +1,22 @@
+import sys
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def scrape_ukutabs(base_url, params):
+    search_page = requests.get(base_url, params=params)
+    soup = BeautifulSoup(search_page.content, "html.parser")
+
+    chords_link = soup.select_one("ul.archivelist li a")["href"]
+
+    return chords_link
+
+
 def main():
-    pass
+    song = " ".join(sys.argv[1:])
+    print(scrape_ukutabs("https://ukutabs.com/", {"s": song}))
+
 
 if __name__ == "__main__":
     main()
