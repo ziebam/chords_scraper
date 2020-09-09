bbbeaa21ab1161df246eac09902c2b21a64fe52d
ziebam <ziebamichal@tutanota.com>
2020-09-09 17:45:28 +0200

    Add support for ukulele-tabs.com

--- chords_scraper/main.py
+++ chords_scraper/main.py
@@ -13,9 +13,26 @@ def scrape_ukutabs(base_url, params):
     return chords_link
 
 
+# The search works very poorly on this website.
+def scrape_ukuleletabs(base_url, subdirs, params):
+    search_page = requests.get(base_url + "/".join(subdirs), params=params)
+    soup = BeautifulSoup(search_page.content, "html.parser")
+
+    chords_link = soup.select_one("ul.resp_list li a")["href"]
+
+    return base_url + chords_link[1:]
+
+
 def main():
     song = " ".join(sys.argv[1:])
     print(scrape_ukutabs("https://ukutabs.com/", {"s": song}))
+    print(
+        scrape_ukuleletabs(
+            "https://www.ukulele-tabs.com/",
+            ["search-uke-chords"],
+            {"find": song},
+        )
+    )
 
 
 if __name__ == "__main__":
