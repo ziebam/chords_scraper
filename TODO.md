85646854d68acf02090dfa45aeb6941f2becaebd
ziebam <ziebamichal@tutanota.com>
2020-09-11 19:32:22 +0200

    Refactor to prepare for a GUI

--- TODO.md
+++
@@ -1,7 +0,0 @@
-[X] https://www.ultimate-guitar.com/
-
-[X] https://www.chords.pl/
-
-[X] https://www.e-chords.com/
-
-[] Handle no results.
--- chords_scraper/main.py
+++ chords_scraper/main.py
@@ -16,7 +16,7 @@ def scrape_ukutabs(base_url, params):
     try:
         chords_link = soup.select_one("ul.archivelist li a")["href"]
     except TypeError:
-        sys.stderr.write("Didn't get any results from `ukutabs.com`.")
+        sys.stderr.write("Didn't get any results from `ukutabs.com`.\n")
         return
 
     return chords_link
@@ -30,7 +30,7 @@ def scrape_ukuleletabs(base_url, subdirs, params):
     try:
         chords_link = soup.select_one("ul.resp_list li a")["href"]
     except TypeError:
-        sys.stderr.write("Didn't get any results from `ukulele-tabs.com`.")
+        sys.stderr.write("Didn't get any results from `ukulele-tabs.com`.\n")
         return
 
     return base_url + chords_link[1:]
@@ -43,7 +43,7 @@ def scrape_echords(base_url, subdirs):
     try:
         chords_link = soup.select_one("ul#results p.h1 a")["href"]
     except TypeError:
-        sys.stderr.write("Didn't get any results from `e-chords.com`.")
+        sys.stderr.write("Didn't get any results from `e-chords.com`.\n")
         return
 
     return chords_link
@@ -64,10 +64,10 @@ def scrape_ultimateguitar(base_url, subdirs, params):
     chords_links = soup.select("div._3gmWN a._2KJtL._1mes3.kWOod")
     for chords_link in chords_links:
         split = chords_link["href"].split("/")
-        if split[3] != "pro":
+        if split[3] != "pro" and split[4] != "misc-your-songs":
             return chords_link["href"]
 
-    sys.stderr.write("Didn't get any results from `ultimate-guitar.com`.")
+    sys.stderr.write("Didn't get any results from `ultimate-guitar.com`.\n")
     return
 
 
@@ -95,7 +95,7 @@ def scrape_chordspl(base_url, artist, title):
                 lambda driver: driver.find_element_by_class_name("v0") != default_result
             )
         except TimeoutException:
-            sys.stderr.write("Didn't get any results from `chords.pl`.")
+            sys.stderr.write("Didn't get any results from `chords.pl`.\n")
             return
 
         result = driver.find_element_by_class_name("v0")
@@ -110,25 +110,23 @@ def main():
     artist = sys.argv[1]
     title = sys.argv[2]
 
-    print(scrape_ukutabs("https://ukutabs.com/", {"s": f"{artist} {title}"}))
-
-    print(
+    links = [
+        scrape_ukutabs("https://ukutabs.com/", {"s": f"{artist} {title}"}),
         scrape_ukuleletabs(
             "https://www.ukulele-tabs.com/",
             "search-uke-chords",
             {"find": title},
-        )
-    )
-
-    print(scrape_echords("https://www.e-chords.com/", f"search-all/{artist} {title}"))
-
-    print(
+        ),
+        scrape_echords("https://www.e-chords.com/", f"search-all/{artist} {title}"),
         scrape_ultimateguitar(
             "https://www.ultimate-guitar.com/", "search.php", f"{artist} {title}"
-        )
-    )
+        ),
+        scrape_chordspl("https://www.chords.pl/", artist, title),
+    ]
 
-    print(scrape_chordspl("https://www.chords.pl/", artist, title))
+    for link in links:
+        if link:
+            print(link)
 
 
 if __name__ == "__main__":
